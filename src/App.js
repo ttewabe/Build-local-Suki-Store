7c11b723c37912ec02db21181f637444cad0b4f0
Tewabe Tadege <ethiohabesha412@gmail.com>
2023-12-15 23:49:48 -0800

    create components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from 'react'
+import {Button, Container} from '@mui/material'
 
 function App() {
+
+  useEffect(()=>{
+document.title = "React material Ui - Home";
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container
+    maxWidth="xl"
+    sx={{
+      background:"#fff"
+    }}
+    >
+      <Button variant='contained'>Test</Button>
+    </Container>
   );
 }
 
