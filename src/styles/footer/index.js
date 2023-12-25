4459a09749b69619dc2ca76e916929b5e2305c8f
Tewabe Tadege <ethiohabesha412@gmail.com>
2023-12-24 23:54:15 -0800

    added footer details

--- src/styles/footer/index.js
+++ src/styles/footer/index.js
@@ -1,8 +1,18 @@
-import { Typography } from "@mui/material";
-import {styled} from '@mui/system';
+import { TextField, Typography } from "@mui/material";
+import styled from '@emotion/styled'
+import { Colors } from "../theme";
 
 
 export const FooterTitle = styled(Typography)(()=>({
 textTransform:'uppercase',
 marginBottom:'1em'
 }));
+
+export const SubscribeTf = styled(TextField)(()=>({
+  '.MuiInputLabel-root':{
+    color:Colors.secondary
+  },
+  '.MuiInputLabel-root::before':{
+    borderBottom:`1px solid ${Colors.secondary}`
+  }
+}))
